953529ec36855dd6f685deee3d17b3052242d009
heavymental33 <non_phixion418@hotmail.com>
2020-07-17 16:26:51 -0400

    Add verification and addinput function + 2/5 section eventlistener

--- app/app.js
+++ app/app.js
@@ -8,13 +8,28 @@ var testing = [];
 var projectManagement = [];
 
 
-
-
+// ES6 function to look if the user enter a value
+const EmptyValue = (value) => {
+    if(value === "" || value === null || value === undefined) {
+        return true
+    } else {
+        return false
+    }
+}
 
 // Take the value from the user input text box
-function addInput() {
-    planification.push(document.getElementById("planning-input").value);
+function addInput(section, arr) {
+    arr.push(section);
     console.log(planification);
+    console.log(design);
+}
+
+function delInput() {
+
+}
+
+function retreiveData(plan, dsg, pro, test, manage) {
+
 }
 
 
@@ -26,6 +41,23 @@ function addInput() {
 // Send input from section text to array
 
 document.getElementById("add-planning").addEventListener("click", function(){
-     addInput();
+    var x = document.getElementById("planning-input").value;
+    if(EmptyValue(x) == true) {
+        alert("Invalid! Please enter a value");
+    } else {
+        addInput(x, planification);
+        document.getElementById("planning-input").value = "";     
+    }   
 
+
+  });
+
+  document.getElementById("add-design").addEventListener("click", function(){
+    var x = document.getElementById("design-input").value;
+    if(EmptyValue(x) == true) {
+        alert("Invalid! Please enter a value");
+    } else {
+     addInput(x, design);
+     document.getElementById("design-input").value = "";
+    }
   });
