0cb2017ca2dbb09635fd7afd094628b3720b5936
heavymental33 <non_phixion418@hotmail.com>
2020-11-30 12:24:55 -0500

    Change some css class name for logic purpose + complete localstorage recup function

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # to-do-list
 A simple To-Do list for manage I.T projects
+JS Vanilla and local storage
 
 
 ## Update to come
-- [ ] Add the local storage system
+- [x] Add the local storage system
 - [ ] Add the priority task
 - [x] Add the function to delete a task
-- [ ] Make the function that will loop through the local storage for load the informations saved
+- [x] Make function that will loop through the local storage for load the informations saved
+- [ ] Update the emptyValue function 
 - [ ] Eventually add a input where the user can put a timer for a task
 - [ ] Make the app fully responsive
 - [ ] Correct some design elements
--- app/app.js
+++ app/app.js
@@ -7,9 +7,30 @@ var projectManagement = [];
 //localStorage.clear();
 console.log(localStorage.getItem("planification"));
 console.log(localStorage.getItem("design"));
+console.log(localStorage.getItem("production"));
 
+// A FAIRE! boucle qui va récupéré les données dans le local storage et va les affiché quand l'utilisateur va revenir ou rechargé la page.
+// Call the function of initiation when the user load the page
+function init() {
+    loadStorage("planification");
+    loadStorage("production");
+}
 
-// function to look if the user enter a value
+init();
+// function with a loop that load the localstorage data into the page when a user come back
+function loadStorage(section) {
+    if(localStorage.getItem(section) !== null && localStorage.getItem(section) !== "[]" ) {
+        var el = document.getElementById("list-" + section);
+        var clef = localStorage.getItem(section);
+        for(var i = 0; i < clef[i].length; i++) {
+            el.innerHTML += '<li class="list-group-item mt-3 plan">' + clef +
+                            '<i class="fas fa-check-circle space-icons"></i><i class="fas fa-eraser space-icons" id="del"></i>'
+                            '</li>';
+                        }
+    }   
+}
+
+// function to validate if the user has enter a valid value (*** À amméliorer ***)
 function emptyValue(value) {
     if (value === "" || value === null || value === undefined || value.indexOf("  ") >= 0 || value === " ") {
         return true;
@@ -28,19 +49,9 @@ function addInput(section, arr) {
     console.log(projectManagement);
 }
 
-function delInput(section, arr) {
-    arr.pop([section]);
-    console.log(planification);
-    console.log(design);
-    console.log(production);
-    console.log(testing);
-    console.log(projectManagement);
-}
-
 function injectList(arr) {
-    //let last = arr[arr.length - 1];
     if(arr === planification) {
-		var li = document.getElementById("list-planning");
+		var li = document.getElementById("list-planification");
         li.innerHTML += '<li class="list-group-item mt-3 plan">' + arr.slice(-1)[0] + 
                         '<i class="fas fa-check-circle space-icons"></i><i class="fas fa-eraser space-icons" id="del"></i>'
                         '</li>';
@@ -106,7 +117,7 @@ function searchArrays(elem,arr) {
         saveData("pm");
     } else {
         console.log("No data!");
-        alert("No data!");
+        alert("No data found!");
     }
 }
 
@@ -130,10 +141,10 @@ document.getElementById("add-design").addEventListener("click", function(){
     if(emptyValue(x) == true) {
         alert("Invalid! Please enter a value");
     } else {
-     addInput(x, design);
-     document.getElementById("design-input").value = "";
-     injectList(design);
-     saveData("design");
+        addInput(x, design);
+        document.getElementById("design-input").value = "";
+        injectList(design);
+        saveData("design");
     }
 });
 
@@ -142,10 +153,10 @@ document.getElementById("add-production").addEventListener("click", function(){
     if(emptyValue(x) == true) {
         alert("Invalid! Please enter a value");
     } else {
-     addInput(x, production);
-     document.getElementById("production-input").value = "";
-     injectList(production);
-     saveData("production");
+        addInput(x, production);
+        document.getElementById("production-input").value = "";
+        injectList(production);
+        saveData("production");
     }
 });
 
@@ -154,10 +165,10 @@ document.getElementById("add-testing").addEventListener("click", function(){
     if(emptyValue(x) == true) {
         alert("Invalid! Please enter a value");
     } else {
-     addInput(x, testing);
-     document.getElementById("testing-input").value = "";
-     injectList(testing);
-     saveData("testing");
+        addInput(x, testing);
+        document.getElementById("testing-input").value = "";
+        injectList(testing);
+        saveData("testing");
     }
 });
 
@@ -166,16 +177,15 @@ document.getElementById("pm-testing").addEventListener("click", function(){
     if(emptyValue(x) == true) {
         alert("Invalid! Please enter a value");
     } else {
-     addInput(x, projectManagement);
-     document.getElementById("pm-input").value = "";
-     injectList(projectManagement);
-     saveData("pm");
+        addInput(x, projectManagement);
+        document.getElementById("pm-input").value = "";
+        injectList(projectManagement);
+        saveData("pm");
     }
 });
 
 // Delete and Done buttons
 document.addEventListener('click',function(e){
-    // var elem = document.getElementById(e.target);
     if(e.target && e.target.id == 'del'){
         var close = document.getElementsByClassName("space-icons");
         var i;
@@ -185,7 +195,7 @@ document.addEventListener('click',function(e){
             var val = div.innerText;
             if(div.className.search("plan") !== -1) {
                 searchArrays(val,"planification");
-                //saveData("planification"); 
+                saveData("planification"); 
             } else if(div.className.search("des") !== -1) {
                 searchArrays(val,"design");
                 saveData("design");
